18246f0ae3bbcdb54ae66f20c2566f4da16596a1
mutasem <mutasem.halawani@gmail.com>
2017-07-22 03:36:42 +0300

    add animations

--- js/script.js
+++ js/script.js
@@ -12,54 +12,49 @@ $('button').on('click',function(e){
 
 function buildQuote(){
     var quoteIndex = Math.floor(Math.random() * (quotes.length));
-    $("#quote-text").text(quotes[quoteIndex].quote);
-    $("#quote-author").text(quotes[quoteIndex].author);
     
+    $("#quote-text").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $("#quote-text").text(quotes[quoteIndex].quote);
+        });
+
+      $("#quote-author").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $("#quote-author").text(quotes[quoteIndex].author);
+        });
+    
+    addQuoteToTwitterIcon(quoteIndex);
     changeColors();
 }
 
-function addQuoteToTwitterIcon(quoteIndex){
+function addQuoteToTwitterIcon(quote){
     $("#twitter").attr("href", "https://twitter.com/intent/tweet?text=" + 
-            quotes[quoteIndex].quote + ' - by ' + quotes[quoteIndex].author);
+            quotes[quote].quote + ' - by ' + quotes[quote].author);
 }
 
 
 function changeColors(){
     var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-//    $("#quote-text").animate({
-//              opacity: 0
-//            }, 500);
 //
-//              $('#quote-text').animate({
-//                opacity: 1
-//              }, 500);
-//
-//          $(".quote-author").animate({
-//              opacity: 0
-//            }, 500,
-//            function() {
-//              $(this).animate({
-//                opacity: 1
-//              }, 500);
-//              $('#author').html(r.author);
-//            });
-//
-          var color = Math.floor(Math.random() * colors.length);
-          console.log(color);
-           $("html body").css('background-color',colors[color]);
-           $("div.quote").css('color',colors[color]);
-           $("button").css('background-color',colors[color]);
-          $("html body").animate({
-            backgroundColor: colors[color],
-            color: colors[color]
-          }, 1000);
-          $("button").animate({
-            backgroundColor: colors[color]
-          }, 1000);
-    //    }
-//      });
-    
-    
+    var color = Math.floor(Math.random() * colors.length);
+      $("button").animate({
+         backgroundColor: colors[color]
+      }, 1000);
+
+      $("html body").animate({
+        backgroundColor: colors[color],
+        color: colors[color]
+      }, 1000);
 }
 
 
